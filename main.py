d75a0997b1589f5f9576058bfaa8fbad3411a1f9
구구공 <orcaleet@gmail.com>
2025-05-18 21:06:04 +0900

    Init

---
+++ main.py
@@ -0,0 +1,11 @@
+class srand:
+    def __init__(self, seed = 523):
+        self.state = seed
+
+    def rand(self):
+        self.state = (214013 * self.state + 2531011) & 0xFFFFFFFF
+        return (self.state >> 16) & 0x7FFF
+
+random = srand(7355608)
+for i in range(10):
+    print(random.rand())
